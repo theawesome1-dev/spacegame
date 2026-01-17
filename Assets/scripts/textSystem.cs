c6f785682fe214887e17978c7162ed261e024478
theawesome1-dev <64999062+theawesome1-dev@users.noreply.github.com>
2026-01-17 18:03:05 -0500

    tmp

--- Assets/scripts/textSystem.cs
+++ Assets/scripts/textSystem.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
-
+using TMPro;
+using System.Collections;
 public class textSystem : MonoBehaviour
 {
+
+    public string[] lines;
+    public TextMeshProUGUI gui;
+    public float textSpeed;
+    private int index;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gui.text = string.Empty;
+        startDialog();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    void startDialog()
+    {
+        index = 0;
+        StartCoroutine(typeLine());
+
+    }
+     IEnumerator typeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            gui.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
     }
 }
