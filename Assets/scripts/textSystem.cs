3b111bf38c5e875ea100f9c40d2e1457a4b9797f
theawesome1-dev <64999062+theawesome1-dev@users.noreply.github.com>
2026-01-17 23:29:28 -0500

    text input

--- Assets/scripts/textSystem.cs
+++ Assets/scripts/textSystem.cs
@@ -1,37 +1,83 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+
+using UnityEngine.Scripting.APIUpdating;
+using Unity.VisualScripting;
+using NUnit.Framework;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
 public class textSystem : MonoBehaviour
 {
 
     public string[] lines;
     public TextMeshProUGUI gui;
     public float textSpeed;
-    private int index;
+    private int index = 0;
+    public bool isFinished = false;
+    public GameObject pannelText;
+    InputSystem_Actions input;
+    private bool inputting = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gui.text = string.Empty;
-        startDialog();
+                gui.text = string.Empty;
+    input = new InputSystem_Actions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        input.Player.Enable();
+        if (index < lines.Length && isFinished == false && inputting == false)
+        {
+            if (index == 0)
+            {
+                startDialog();
+
+            }
+            if (index > 0 && input.Player.Interact.WasReleasedThisFrame())
+            {
+                startDialog();
+            }
+        }
+        if (index == lines.Length && input.Player.Interact.WasReleasedThisFrame())
+        {
+            pannelText.SetActive(false);
+            isFinished = true;
+        }
+    }
+    public void startText(string[] texts)
+    {
+        lines = texts;
+        inputting = false;
+        isFinished = false;
+        pannelText.SetActive(true);
+        index  = 0;
+        gui.text = string.Empty;
+
     }
     void startDialog()
     {
-        index = 0;
         StartCoroutine(typeLine());
+        
 
     }
      IEnumerator typeLine()
     {
+        inputting = true;
+        gui.text = string.Empty;
+
         foreach (char c in lines[index].ToCharArray())
         {
             gui.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
+        inputting = false;
+        index++;
+
     }
 }
