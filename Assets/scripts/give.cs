03afa4d252b23ec92086c7d5fe7b631b17fb589c
theawesome1-dev <64999062+theawesome1-dev@users.noreply.github.com>
2026-01-18 09:21:19 -0500

    text

---
+++ Assets/scripts/give.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class give : MonoBehaviour
+{
+    public textSystem textSystem;
+    public string[] text;
+    InputSystem_Actions input;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+         input = new InputSystem_Actions();
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider other)
+    {
+        input.Player.Enable();
+
+        if (other.tag == "Player")
+        {
+            Debug.Log("plate");
+            if (input.Player.Interact.WasReleasedThisFrame() && textSystem.isFinished == true)
+            {
+                Debug.Log("Start text");
+
+                textSystem.startText(text);
+            }
+        }
+    }
+}
